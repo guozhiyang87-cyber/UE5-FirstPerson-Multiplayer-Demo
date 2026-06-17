99c8de1827698466d5866a1ca404faf9af75dc43
guozhiyang87-cyber <guozhiyang87@gmail.com>
2026-06-17 11:00:05 +0800

    Update FirstPersonDemo.cpp
    定义 LogFirstPersonDemo 函数

--- Source/FirstPersonDemo.cpp
+++ Source/FirstPersonDemo.cpp
@@ -1,4 +1,9 @@
 #include "FirstPersonDemo.h"
 #include "Modules/ModuleManager.h"
+#include "CoreMinimal.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, FirstPersonDemo, "FirstPersonDemo");
+
+DEFINE_LOG_CATEGORY(LogFirstPersonDemo);
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstPersonDemo, "FirstPersonDemo" );
